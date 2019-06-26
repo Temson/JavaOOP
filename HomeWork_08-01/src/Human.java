378048d5c8b4b1f5f6121a66106306ed2ae9c534
Temson <artemson1@gmail.com>
2019-06-26 14:40:04 +0300

    Update Human.java

--- HomeWork_08-01/src/Human.java
+++ HomeWork_08-01/src/Human.java
@@ -1,6 +1,7 @@
 import java.io.Serializable;
 
 public class Human implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String name;
     private String surname;
     private int age;
